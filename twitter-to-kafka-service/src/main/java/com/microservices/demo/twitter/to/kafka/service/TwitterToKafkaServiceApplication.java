83bb7bbb772a103029b84af56e3729a686d31fc8
Filippo Mortari <filippo.mortari@gmail.com>
2021-03-04 21:02:28 +0000

    8. Adding mock twitter stream as an alternative

--- twitter-to-kafka-service/src/main/java/com/microservices/demo/twitter/to/kafka/service/TwitterToKafkaServiceApplication.java
+++ twitter-to-kafka-service/src/main/java/com/microservices/demo/twitter/to/kafka/service/TwitterToKafkaServiceApplication.java
@@ -6,10 +6,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
 @Slf4j
-@EnableScheduling
 @SpringBootApplication
 public class TwitterToKafkaServiceApplication implements CommandLineRunner {
 
